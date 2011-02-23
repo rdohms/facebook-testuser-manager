a6c824e3944bcb0697841d18163ec7de2a8656af
Rafael Dohms <rdohms@gmail.com>
2011-02-23 16:27:52 -0300

    Bumping version for bug fix release

--- library/bootstrap.php
+++ library/bootstrap.php
@@ -4,7 +4,7 @@ define('ROOT_PATH', __DIR__ . '/../');
 define('LIB_PATH', ROOT_PATH . 'library/');
 define('TPL_PATH', ROOT_PATH . 'templates/');
 
-define('APP_VERSION', '0.9.3');
+define('APP_VERSION', '0.9.4');
 
 //Define library folders
 $includePaths = array();
