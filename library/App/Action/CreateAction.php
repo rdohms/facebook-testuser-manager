9a298dcd6c8d4c7ce95c2fd620816e668a3d91b4
Diego Oliveira <contato@diegoholiveira.com>
2012-01-24 18:08:58 -0200

    * Adding a default configuration to the user permissions; * Displaying the basic informations for the created used (password can only be displayed on this moment)

--- library/App/Action/CreateAction.php
+++ library/App/Action/CreateAction.php
@@ -16,13 +16,18 @@ class CreateAction extends Base
 
             $user = $fb->api('/'.$fb->getAppId().'/accounts/test-users', 'POST', $params);
 
-            if (is_array($user)){
+            // check if the user has been created successfully
+            if (is_array($user)) {
+                $success = true;
+            }
 
+            // get more informations about the user
+            // case if they already installed on your app
+            if ($params['installed']) {
                 $fb->setAccessToken($user['access_token']);
                 $details = $fb->api('/me');
 
                 $user = \array_merge($user,$details);
-                $success = true;
             }
         } catch (\Exception $e) {
             $this->redirectToError($e);
@@ -35,5 +40,3 @@ class CreateAction extends Base
 	}
 	
 }
-
-?>
--- library/App/Action/SetSessionAction.php
+++ library/App/Action/SetSessionAction.php
@@ -8,18 +8,21 @@ class SetSessionAction extends Base
 	public function run()
 	{
         //Check submitted
-        $appId = $this->getInspekt()->post->getAlnum('fb_app_id');
-        $appSecret = $this->getInspekt()->post->getAlnum('fb_app_secret');
+        $appId          = $this->getInspekt()->post->getAlnum('fb_app_id');
+        $appSecret      = $this->getInspekt()->post->getAlnum('fb_app_secret');
+        $appPermissions = $this->getInspekt()->post->getAlnum('fb_app_permissions');
 
         
         if ($appId) $_SESSION['FACEBOOK_APP_ID'] = $appId;
         if ($appSecret) $_SESSION['FACEBOOK_APP_SECRET'] = $appSecret;
+        if ($appPermissions) $_SESSION['FACEBOOK_APP_DEFAULT_PERMISSIONS'] = $appPermissions;
 
         //Check for clear session
         $submitAction = $this->getInspekt()->post->getAlnum('action');
         if($submitAction == "ClearcurrentAppID"){
             unset($_SESSION['FACEBOOK_APP_ID']);
             unset($_SESSION['FACEBOOK_APP_SECRET']);
+            unset($_SESSION['FACEBOOK_APP_DEFAULT_PERMISSIONS']);
         }
         
         //Check Session
@@ -36,7 +39,8 @@ class SetSessionAction extends Base
 		$tpl = $this->getTplEngine()->loadTemplate('set_session.html');
         $tpl->display(array(
             'appId' => $appId, 
-            'appSecret' => $appSecret, 
+            'appSecret' => $appSecret,
+            'appPermissions' => $appPermissions,
             'sessionAppId' => $sessionAppId,
             'noSession' => $noSession
         ));
@@ -44,5 +48,3 @@ class SetSessionAction extends Base
 	}
 	
 }
-
-?>
